7da43f58339a0d3f201835861f7c4e7c38bf367b
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-06-01 10:19:06 +0530

    updated the api

--- index.js
+++ index.js
@@ -8,20 +8,45 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 
-app.get("/api", async (req, res) => {
-  const url = `${process.env.GET_API}`;
+// app.get("/api", async (req, res) => {
+//   const url = `${process.env.GET_API}`;
+//   try {
+//     const response = await axios.get(url, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Origin": "*",
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+//       },
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+app.post('/submit-form', async (req, res) => {
+  const apiUrl = 'https://app.shootorder.com/items/contacts/?access_token=hes4c7izEe7Rsx4C0GkP4KKBeNqB9JJv';
+
   try {
-    const response = await axios.get(url, {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify(req.body),
     });
-    res.json(response.data);
+
+    if (response.ok) {
+      console.log('Form submitted successfully!');
+      res.sendStatus(200);
+    } else {
+      console.error('Error submitting form:', response.statusText);
+      res.sendStatus(response.status);
+    }
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    console.error('Error submitting form:', error);
+    res.sendStatus(500);
   }
 });
 
